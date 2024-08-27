ff0ca2f198dee2596d57f101576962d4e834159d
Mateus Rocha Pereira <mateus.pereira@f1rst.com.br>
2024-08-26 23:31:19 -0300

    feat: include method adicionar on carrinho

--- src/app/model/carrinho.ts
+++ src/app/model/carrinho.ts
@@ -3,4 +3,9 @@ import { Produto } from "./produto";
 export class Carrinho {
   public produtos: Produto[] = [];
   public valorTotal: number = 0;
+
+  public adiciona(produto: Produto): void {
+    this.produtos.push(produto);
+    this.valorTotal += produto.preco;
+  }
 }
